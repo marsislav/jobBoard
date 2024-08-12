dc15c0f42a7514f077ae0cccfd7b3aaf7fdc035c
marsislav <mkostadinov87@gmail.com>
2024-08-12 11:37:24 +0300

    Added functionality for job info, job individual page

---
+++ resources/views/components/link-button.blade.php
@@ -0,0 +1,3 @@
+<a href="{{$href}}"
+        class="rounded-md border border-slate-300 text-black hover:bg-slate-100 shadow-sm text-sm font-semibold text-center bg-white px-2.5 py-1.5"
+        >{{$slot}}</a>
--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,5 +1,10 @@
 <x-layout>
-    @foreach ($jobs as $job)
-    <div>{{$job->title}}</div>
-    @endforeach
+  @foreach ($jobs as $job)
+    <x-job-card class="mb-4" :job="$job">
+    <div>
+        
+        <x-link-button :href="route('jobs.show', $job)">Show</x-link-button>
+      </div>
+    </x-job-card>
+  @endforeach
 </x-layout>
