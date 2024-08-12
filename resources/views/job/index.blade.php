6c48a513cf2c14c02cacf5023c9d4cf3095dcfa1
marsislav <mkostadinov87@gmail.com>
2024-08-12 15:09:07 +0300

    Added text search functionality

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -3,6 +3,7 @@
     :links="['Jobs' => route('jobs.index')]" />
 
   <x-card class="mb-4 text-sm">
+    <form action="{{route('jobs.index')}}" method="GET">
     <div class="mb-4 grid grid-cols-2 gap-4">
       <div>
         <div class="mb-1 font-semibold">Search</div>
@@ -19,6 +20,8 @@
       <div>3</div>
       <div>4</div>
     </div>
+    <button class="w-full">Filter</button>
+    </form>
   </x-card>
 
   @foreach ($jobs as $job)
