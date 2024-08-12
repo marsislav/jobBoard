de8488268f334356608d8303db5130de5d13a2be
marsislav <mkostadinov87@gmail.com>
2024-08-12 21:27:33 +0300

    Installed AlpineJS, styling. Fixed bugs

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,11 +4,13 @@
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
+import axios from "axios";
+import Alpine from "alpinejs";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+window.Alpine = Alpine;
+Alpine.start();
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
